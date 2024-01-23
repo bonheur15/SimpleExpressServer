da89c3bd29712bf1e6070d8d2d8e8a9d6be1254c
bonheur15 <rumanzibonheur@gmail.com>
2024-01-23 09:04:10 +0200

    changed the config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,12 @@ module.exports = {
     instances:1,
     exec_mode:"cluster"
   }],
-
   deploy : {
     production : {
       user : 'root',
       host : '164.90.154.134',
       ref  : 'origin/master',
-      repo : 'https://github.com/bonheur15/SimpleExpressServer.git',
+      repo : 'git@github.com:bonheur15/SimpleExpressServer.git',
       path : '/home/app',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
